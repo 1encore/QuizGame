73a542827e006af1fd62d50851750be5adb66897
ENCORE-PC <hg1zadr@gmail.com>
2018-07-16 22:47:47 +0600

    Working lifecycle

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,14 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     public Question[] questions;
-    private static
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    [SerializeField]
+    private Text questionText;
+
+    [SerializeField]
+    private Text trueAnswerText;
+
+    [SerializeField]
+    private Text falseAnswerText;
+
+    [SerializeField]
+    private float delay = 1f;
+
+    [SerializeField]
+    private Animator animator;
 
     void Start ()
     {
+        if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+        SetCurrentQuestion();
+    }
 
+    void SetCurrentQuestion()
+    {
+        int randomQuestionIndex = Random.Range(0,unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        questionText.text = currentQuestion.question;
+
+        if (currentQuestion.isTrue)
+        {
+            trueAnswerText.text = "Correct";
+            falseAnswerText.text = "Wrong";
+        }
+        else
+        {
+            trueAnswerText.text = "Wrong";
+            falseAnswerText.text = "Correct";
+        }
+    }
+
+    IEnumerator TransitionToNextQuestion ()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+
+        yield return new WaitForSeconds(delay);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void UserSelectTrue()
+    {
+        animator.SetTrigger("True");
+
+        if (currentQuestion.isTrue)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+    public void UserSelectFalse()
+    {
+        animator.SetTrigger("False");
+
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
     }
 }
